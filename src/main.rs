2337457432445ba004376b56607c00847ddbdff7
Tim Dévé <timdeve@users.noreply.github.com>
2018-11-26 14:32:46 +0000

    Selects files and output them

--- src/main.rs
+++ src/main.rs
@@ -29,29 +29,25 @@ enum_from_primitive! {
 struct File {
     status: String,
     path: String,
-    isSelected: bool,
+    is_selected: bool,
 }
 
 fn main() {
+    let mut cursor_position = 0;
     let original_termios = Termios::from_fd(STDIN_FILENO).unwrap();
 
     let output = get_git_status();
 
     if output.status.success() {
-        let files = marshal_status_in_files(
+        let mut files = marshal_status_in_files(
             String::from_utf8(output.stdout).expect("Problem parsing status"),
         );
 
-        display(fmt_files_to_strings(files));
-
-        println!("");
-
         loop {
-            print!("\x1b[1F");
-            print!("\x1b[1G");
-
+            display(add_cursor(cursor_position, fmt_files_to_strings(&files)));
+            println!("");
             set_terminal_to_raw();
-            read_key(&original_termios).unwrap();
+            read_key(&original_termios, &mut cursor_position, &mut files).unwrap();
             reset_terminal(&original_termios);
         }
     } else {
@@ -73,7 +69,7 @@ fn marshal_status_in_files(status: String) -> Vec<File> {
         .map(|line| File {
             status: line[0..2].to_string(),
             path: line[3..].to_string(),
-            isSelected: false,
+            is_selected: false,
         }).collect()
 }
 
@@ -87,13 +83,27 @@ fn reset_terminal(original_termios: &Termios) {
     tcsetattr(STDIN_FILENO, TCSANOW, original_termios).unwrap();
 }
 
-fn fmt_files_to_strings(files: Vec<File>) -> Vec<String> {
+fn add_cursor(cursor_position: usize, lines: Vec<String>) -> Vec<String> {
+    let mut new_lines: Vec<String> = vec![];
+
+    for (i, line) in lines.iter().enumerate() {
+        new_lines.push(format!(
+            "{} {}",
+            if cursor_position == i { ">" } else { " " },
+            line
+        ))
+    }
+
+    new_lines
+}
+
+fn fmt_files_to_strings(files: &Vec<File>) -> Vec<String> {
     files
         .iter()
         .map(|file| {
             format!(
                 "[{}] {} {}",
-                if file.isSelected { "*" } else { " " },
+                if file.is_selected { "*" } else { " " },
                 file.status,
                 file.path
             )
@@ -106,7 +116,20 @@ fn display(lines: Vec<String>) {
     }
 }
 
-fn read_key(original_termios: &Termios) -> io::Result<()> {
+fn join_selected_files(files: &Vec<File>) -> String {
+    files
+        .iter()
+        .filter(|file| file.is_selected)
+        .map(|file| file.path.clone())
+        .collect::<Vec<String>>()
+        .join(" ")
+}
+
+fn read_key(
+    original_termios: &Termios,
+    cursor_position: &mut usize,
+    files: &mut Vec<File>,
+) -> io::Result<()> {
     let stdout = io::stdout();
     let mut reader = io::stdin();
     let mut buffer = [0; 3];
@@ -120,8 +143,20 @@ fn read_key(original_termios: &Termios) -> io::Result<()> {
 
     if let Some(key) = Keys::from_u8(buffer[0]) {
         match key {
-            Keys::J => println!("J"),
-            Keys::K => println!("K"),
+            Keys::J => {
+                if *cursor_position == files.len() - 1 {
+                    *cursor_position = 0;
+                } else {
+                    *cursor_position = *cursor_position + 1;
+                }
+            }
+            Keys::K => {
+                if *cursor_position == 0 {
+                    *cursor_position = files.len() - 1;
+                } else {
+                    *cursor_position = *cursor_position - 1;
+                }
+            }
             Keys::Q | Keys::Escape => {
                 reset_terminal(original_termios);
                 process::exit(0);
@@ -130,8 +165,15 @@ fn read_key(original_termios: &Termios) -> io::Result<()> {
                 reset_terminal(original_termios);
                 process::exit(130);
             }
-            Keys::Enter => println!("Enter"),
-            Keys::Space => println!("Space"),
+            Keys::Enter => {
+                reset_terminal(original_termios);
+                let paths = join_selected_files(&files);
+                println!("{}", paths);
+                process::exit(0);
+            }
+            Keys::Space => {
+                files[*cursor_position].is_selected = !files[*cursor_position].is_selected
+            }
         }
     }
 
@@ -152,12 +194,12 @@ mod tests {
                 File {
                     status: String::from(" M"),
                     path: String::from("src/main.rs"),
-                    isSelected: false,
+                    is_selected: false,
                 },
                 File {
                     status: String::from("??"),
                     path: String::from("wow"),
-                    isSelected: false,
+                    is_selected: false,
                 },
             ],
             files
@@ -170,21 +212,61 @@ mod tests {
             File {
                 status: String::from("??"),
                 path: String::from("/hello"),
-                isSelected: true,
+                is_selected: true,
             },
             File {
                 status: String::from(" M"),
                 path: String::from("/is-it-me-you're-looking-for"),
-                isSelected: false,
+                is_selected: false,
             },
         ];
 
         assert_eq!(
-            fmt_files_to_strings(files),
+            fmt_files_to_strings(&files),
             vec![
                 String::from("[*] ?? /hello"),
                 String::from("[ ]  M /is-it-me-you're-looking-for")
             ]
+        )
+    }
+
+    #[test]
+    fn add_cursor_to_string() {
+        let lines = vec![
+            "Line 1".to_string(),
+            "Line 2".to_string(),
+            "Line 3".to_string(),
+        ];
+
+        assert_eq!(
+            add_cursor(1, lines),
+            vec!["  Line 1", "> Line 2", "  Line 3"]
+        );
+    }
+
+    #[test]
+    fn joins_selected_files() {
+        let files = vec![
+            File {
+                status: String::from("??"),
+                path: String::from("/hello"),
+                is_selected: true,
+            },
+            File {
+                status: String::from(" M"),
+                path: String::from("/it's-me"),
+                is_selected: false,
+            },
+            File {
+                status: String::from(" M"),
+                path: String::from("/i-was-wondering"),
+                is_selected: true,
+            },
+        ];
+
+        assert_eq!(
+            join_selected_files(&files),
+            "/hello /i-was-wondering".to_string()
         )
     }
 }
